5c95ff4273144c209cf04f87e5dd6cb73185e565
Zoryana Herman <z.herman@ucu.edu.ua>
2021-03-26 14:06:02 +0200

    Configuration parser

--- inc/reading_conf.h
+++ inc/reading_conf.h
@@ -3,12 +3,13 @@
 #ifndef ANALYSIS_SNP_READING_CONF_H
 #define ANALYSIS_SNP_READING_CONF_H
 
-struct rna_configuration{
-    std::string rna_sequence_initial;
-    std::string rna_sequence_to_mutate;
-    int mutation_amount;
-    std::vector<std::string> amino_acids;
-    std::vector<int> mutation_places;
+struct rna_configuration {
+    std::string rna_sequence_initial{};
+    std::string rna_sequence_to_mutate{};
+    int mutation_amount{};
+    std::vector<std::string> amino_acids{};
+    std::vector<int> mutation_places{};
+    static rna_configuration parse_config(const std::string &filename);
 };
 
 #endif //ANALYSIS_SNP_READING_CONF_H
